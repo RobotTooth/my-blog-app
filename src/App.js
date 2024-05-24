9bd606c80db8bdc6336561e23a23d125c8f76ca1
Chris Meysner <110308659+RobotTooth@users.noreply.github.com>
2024-05-24 22:43:40 +0100

    Settings components created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import TopBar from "./components/topbar/TopBar";
+import Settings from "./pages/settings/Settings";
 // import Home from "./pages/home/Home";
 // import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
+// import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Write />
+      <Settings />
     </>
   );
 }
